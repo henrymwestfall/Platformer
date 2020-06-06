c1f23e295e8426a92c32419ad2676c3a9309d996
Henry Westfall <henrymwestfall@gmail.com>
2020-06-06 16:10:29 -0600

    reorganize sprites.player.Player.update into other methods

--- sprites/player.py
+++ sprites/player.py
@@ -12,11 +12,7 @@ class Player(RigidBody):
         RigidBody.__init__(self, scene)
 
         self.image = pg.Surface([32, 52])
-        self.image.fill(BLUE)
-        internal_image = pg.Surface([24, 24])
-        internal_image.fill(RED)
-        rect = pg.Rect(4, 4, 28, 28)
-        self.image.blit(internal_image, rect)
+        self.image.fill(SKY_BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -32,33 +28,33 @@ class Player(RigidBody):
 
         self.score = 0
 
-    def update(self, dt, t):
-        landed = len(self.collisions["down"]) > 0
-        touching_left = len(self.collisions["left"]) > 0
-        touching_right = len(self.collisions["right"]) > 0
+        self.landed = False
+        self.touching_left = False
+        self.touching_right = False
+        self.climbing = False
 
-        # apply gravity
-        if not landed:
+    def apply_gravity(self, dt):
+        if not self.landed:
             self.vel += pg.math.Vector2(0, self.scene.gravity) * dt
 
-        # jump
-        if landed and self.scene.keys_pressed[pg.K_w]:
+    def handle_jumping(self):
+        if self.landed and self.scene.keys_pressed[pg.K_w]:
             self.vel.y = -self.jump_strength
         elif self.vel.y < 0 and not self.scene.keys_pressed[pg.K_w]:
             self.vel.y *= self.jump_cut
 
-        # handle climbing
-        
-        if not landed and (touching_left or touching_right):
+    def handle_climbing(self):
+        self.climbing = False
+        if not self.landed and (self.touching_left or self.touching_right):
+            self.climbing = True
             self.vel.y = 0
             if self.scene.keys_pressed[pg.K_w]:
                 self.vel.y = -self.climb
             elif self.scene.keys_pressed[pg.K_s]:
                 self.vel.y = self.climb
-        
-        # move left/right
-        self.moving = False
 
+    def handle_horz_movement(self):
+        self.moving = False
         if self.scene.keys_pressed[pg.K_d] ^ self.scene.keys_pressed[pg.K_a]:
             if self.scene.keys_pressed[pg.K_d]:
                 self.vel.x = self.vel.lerp(pg.math.Vector2(self.speed, 0), self.acc).x
@@ -67,7 +63,8 @@ class Player(RigidBody):
                 self.vel.x = self.vel.lerp(pg.math.Vector2(-self.speed, 0), self.acc).x
                 self.moving = True
 
-        if landed and not self.moving:
+    def apply_friction(self):
+        if self.landed and not self.moving:
             friction = self.collisions["down"][0].friction * math.copysign(1, -self.vel.x)
             new_vel_x = self.vel.x + friction
             if math.copysign(1, self.vel.x) == math.copysign(1, new_vel_x):
@@ -75,19 +72,26 @@ class Player(RigidBody):
             else:
                 self.vel.x = 0
 
-        self.image = pg.Surface([self.rect.width, self.rect.height])
-        self.image.fill(SKY_BLUE)
-        """
-        internal_image = pg.Surface([24, 24])
-        if self.moving:
-            internal_image.fill(RED)
-        else:
-            internal_image.fill(BLUE)
-        rect = pg.Rect(4, 4, 28, 28)
-        self.image.blit(internal_image, rect)"""
+    def update(self, dt, t):
+        # update collision variables
+        self.landed = len(self.collisions["down"]) > 0
+        self.touching_left = len(self.collisions["left"]) > 0
+        self.touching_right = len(self.collisions["right"]) > 0
 
-        if self.scene.keys_pressed[pg.K_SPACE]:
-            self.scene.shake_screen(t, 50, 2)
+        # apply gravity
+        self.apply_gravity(dt)
+
+        # handle jumping
+        self.handle_jumping()
+
+        # handle climbing
+        self.handle_climbing()
+        
+        # move left/right
+        self.handle_horz_movement()
+
+        # apply friction
+        self.apply_friction()
         
         # update position
         self.move(dt)
@@ -98,6 +102,7 @@ class Player(RigidBody):
             rect_pos.x = math.floor(rect_pos.x)
         self.rect.topleft = rect_pos
 
+        # handle collisions with collectables and other rigid bodies
         collided_rigid_bodies = pg.sprite.spritecollide(self, self.scene.rigid_bodies, False)
         for body in collided_rigid_bodies:
             if isinstance(body, Coin):
